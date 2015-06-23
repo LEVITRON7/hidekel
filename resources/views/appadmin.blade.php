2fb912b69a97158d9553d5d51bdf2b116d74628d
levitron7 <yonny90_15@hotmail.com>
2015-06-23 14:57:27 -0500

    update view appadmin

--- resources/views/appadmin.blade.php
+++ resources/views/appadmin.blade.php
@@ -50,7 +50,7 @@
         @if(Session::has('messages'))
             <div class="alert alert-success">
                 {{Session::get('messages')}}
-                <i class="fa fa-exclamation-circle"></i>
+                <i class="fa fa-check-circle"></i>
                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
